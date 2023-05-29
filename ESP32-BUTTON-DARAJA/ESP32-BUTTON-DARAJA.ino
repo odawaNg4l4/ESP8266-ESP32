61032d77a0e98f032f70eca850b02ee7b141a3d0
odawaNg4l4 <odawangala99@gmail.com>
2023-05-29 11:59:47 +0300

    ESP

---
+++ ESP32-BUTTON-DARAJA/ESP32-BUTTON-DARAJA.ino
@@ -0,0 +1,281 @@
+// Import required libraries
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <AsyncTCP.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
+// Replace with your network credentials
+const char* ssid = "Psalmchmart";
+const char* password = "Psalmucho@2040";
+
+const char* PARAM_INPUT_1 = "state";
+
+const int output = 27;
+const int buttonPin = 26;
+
+// Variables will change:
+int ledState = LOW;          // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50; 
+
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lipa na mpesa</title>
+  
+    <link href="" rel="stylesheet" />
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ">
+   
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
+
+      body {
+        background-color: #eaedf4;
+        font-family: "Rubik", sans-serif;
+      }
+
+      .card {
+        width: 310px;
+        border: none;
+        border-radius: 15px;
+      }
+
+      .justify-content-around div {
+        border: none;
+        border-radius: 20px;
+        background: #f3f4f6;
+        padding: 5px 20px 5px;
+        color: #8d9297;
+      }
+
+      .justify-content-around span {
+        font-size: 12px;
+      }
+
+      .justify-content-around div:hover {
+        background: #545ebd;
+        color: #fff;
+        cursor: pointer;
+      }
+
+      .justify-content-around div:nth-child(1) {
+        background: #545ebd;
+        color: #fff;
+      }
+
+      span.mt-0 {
+        color: #8d9297;
+        font-size: 12px;
+      }
+
+      h6 {
+        font-size: 15px;
+      }
+      .mpesa {
+        background-color: green !important;
+      }
+
+      img {
+        border-radius: 15px;
+      }
+    </style>
+  </head>
+  <body  class="snippet-body">
+    <div class="container d-flex justify-content-center">
+      <div class="card mt-5 px-3 py-4">
+        <div class="d-flex flex-row justify-content-around">
+          <div class="mpesa"><span>Mpesa </span></div>
+          <div><span>Paypal</span></div>
+          <div><span>Card</span></div>
+        </div>
+        <div class="media mt-4 pl-2">
+   
+          <div class="media-body">
+            <h6 class="mt-1">Enter Amount & Number</h6>
+          </div>
+        </div>
+        <div class="media mt-3 pl-2">
+                          <!--bs5 input-->
+
+            <form class="row g-3" action="/api/stkPush" method="POST" enctype="application/x-www-form-urlencoded">
+            
+                <div class="col-12">
+                  <label for="inputAddress" class="form-label">Amount</label>
+                  <input type="text" id="amount" class="form-control" name="amount" placeholder="Enter Amount">
+                </div>
+                <div class="col-12">
+                  <label for="inputAddress2" class="form-label" >Phone Number</label>
+                  <input type="text" class="form-control" id="phone" name="phone"  placeholder="Enter Phone Number">
+                </div>
+             
+                <div class="col-12">
+                  <button type="submit" class="btn btn-success" name="submit" value="submit">Submit</button>
+                </div>
+              </form>
+              <!--bs5 input-->
+          </div>
+        </div>
+      </div>
+    </div>
+    <script>
+      document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  
+  // Collect the form data
+  const amount = document.getElementById("amount").value;
+  const phone = document.getElementById("phone").value;
+  
+  // Create an object with the form data
+  const formData = { amount, phone };
+  
+  // Send an HTTP POST request to the server
+  fetch("/api/stkPush", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (response.ok) {
+      // Request was successful
+      console.log("Data sent successfully");
+    } else {
+      // Request failed
+      console.error("Failed to send data");
+    }
+  })
+  .catch(error => {
+    console.error("An error occurred while sending data", error);
+  });
+});
+    </script>
+  </body>
+</html>
+)rawliteral";
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    String outputStateValue = String(buttonState);
+    buttons+= "<h4>Output - GPIO 2 - State <span id=\"outputState\"></span></h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
+}
+
+String outputState(){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
+  return "";
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+  pinMode(buttonPin, INPUT);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/update?state=<inputMessage>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/update?state=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      digitalWrite(output, inputMessage.toInt());
+      ledState = !ledState;
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Send a GET request to <ESP_IP>/state
+  server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(digitalRead(output)).c_str());
+  });
+  // Start server
+  server.begin();
+}
+  
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(output, ledState);
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
+
+
